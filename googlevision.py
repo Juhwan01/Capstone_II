d56f2109624f3386da7a688938e093f47dd9fc46
Juhwan01 <wnghks5432@naver.com>
2025-01-02 23:56:46 +0900

    01.02 - vision 인식 테스트

---
+++ googlevision.py
@@ -0,0 +1,13 @@
+from google.cloud import vision
+
+def detect_food(image_path):
+    client = vision.ImageAnnotatorClient()
+    
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    
+    return [label.description for label in labels]
