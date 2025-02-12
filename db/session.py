aff59d9e2a3b864b00c209f517e34116921a2155
Juhwan01 <wnghks5432@naver.com>
2025-02-13 02:25:32 +0900

    02.13 - RDS를 이용한 DB 구현

--- db/session.py
+++ db/session.py
@@ -1,13 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
+import ssl
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 engine = create_async_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,
     echo=True,
     future=True,
-    connect_args={"ssl": False}  # SSL 비활성화
+    connect_args={"ssl": ssl_context}  # SSL 컨텍스트 사용
 )
 
 AsyncSessionLocal = sessionmaker(
