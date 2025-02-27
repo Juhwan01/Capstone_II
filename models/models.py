4c063354951d34c6c00d2bc5aa78a0fd41f64097
Jun3671 <wnsgur3027@naver.com>
2025-02-27 22:11:02 +0900

    02.27 - Sale 모델 수정

--- models/models.py
+++ models/models.py
@@ -102,7 +102,7 @@ class Sale(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True, autoincrement=True)  # 판매 고유 ID
     ingredient_name = Column(String)
-    ingredient_id = Column(Integer, ForeignKey('ingredients.id'), nullable=False)  # 식재료 ID (Foreign Key)
+    ingredient_id = Column(Integer, ForeignKey('ingredients.id'), nullable=True)  # 식재료 ID (Foreign Key)
     seller_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # 판매자 ID (Foreign Key)
     value = Column(Float, nullable=False)  # 판매 가격
     location_lat = Column(Float, nullable=False)  # 판매 위치 위도
